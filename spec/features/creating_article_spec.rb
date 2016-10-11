c7d8916b64a7ba1f981f2fab3f9757812946669b
sam0191 <samer.abunuwara@gmail.com>
2016-10-11 21:03:17 +0300

    Create Article successfullu

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -11,6 +11,6 @@ RSpec.feature "Creating Articles" do
 		click_button "Create Article"
 
 		expect(page).to have_content("Article has been created")
-		expect(page.current_path).to eq(article_path)
+		expect(page.current_path).to eq(articles_path)
 	end
 end
