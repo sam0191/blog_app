2b335610a61c11f22a67318f8c7b969872c8030e
sam0191 <samer.abunuwara@gmail.com>
2016-10-12 13:31:49 +0300

    Implement show article feature

---
+++ spec/features/showing_article_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Showing an Article" do
+	
+	before do
+		@article = Article.create(title: "first article", body: "body of first article")
+	end
+
+	scenario "Display individual article" do
+		visit "/"
+
+		click_link @article.title
+
+		expect(page).to have_content(@article.title)
+		expect(page).to have_content(@article.body)
+		expect(current_path).to eq(article_path(@article))
+	end
+end
