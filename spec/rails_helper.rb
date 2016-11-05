e5e4eeb2e83f1a1cb342e30922a918fe6610f3bb
sam0191 <samer.abunuwara@gmail.com>
2016-11-05 14:27:03 +0200

    Restrict access with controller actions

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,9 @@ require 'spec_helper'
 require 'rspec/rails'
 include Warden::Test::Helpers
 Warden.test_mode!
+RSpec.configure do |config|
+  config.include Devise::Test::ControllerHelpers, type: :controller
+end
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
